5e37ba6fa7678d75cd647b5785f6a1f4d7227018
Ewen Cheung <yiwencheung@gmail.com>
2025-04-16 16:57:50 +0800

    push

--- Lab4/frontend/src/pages/Employer/EP_ViewApplicantProfilePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EP_ViewApplicantProfilePage = () => {
-  return (
-    <div>EP_ViewApplicantProfilePage</div>
-  )
-}
-
-export default EP_ViewApplicantProfilePage
--- Lab4/frontend/src/pages/Employer/EP_ViewCandidatesPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EP_ViewCandidatesPage = () => {
-  return (
-    <div>EP_ViewCandidatesPage</div>
-  )
-}
-
-export default EP_ViewCandidatesPage
